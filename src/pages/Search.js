649060e10c736790a89e05050f49a3b14a5015d9
edchavezb <edgp.chavez@gmail.com>
2022-03-27 11:38:04 -0600

    Matches weight in filtering function

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,6 +6,7 @@ import GridView from "../components/elements/GridView";
 function Search({ data = "true" }) {
   const [dogs, setDogs] = useState([]);
   const [nameInput, setNameInput] = useState("")
+  const [filters, setFilters] = useState({minHeight: 5, maxHeight: 120, minWeight: 1, maxWeight: 120, lifespan: null, temperament: []})
   let typingDebouncer;
 
   useEffect(() => {
@@ -15,6 +16,10 @@ function Search({ data = "true" }) {
   useEffect(() => {
     console.log(nameInput)
   }, [nameInput])
+
+  useEffect(() => {
+    console.log(filters)
+  }, [filters])
 
   const getAllBreeds = () => {
     fetch("https://api.thedogapi.com/v1/breeds", {
@@ -38,16 +43,41 @@ function Search({ data = "true" }) {
     typingDebouncer = setTimeout(() => setNameInput(e.target.value), 500);
   }
 
+  const handleFilterChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    let inputValue = e.target.type == "number" ? parseInt(e.target.value) : e.target.value;
+
+    const newFilters = { ...filters, [e.target.name]: inputValue }
+    setFilters(newFilters);
+  }
+
   const matchesUserQuery = (dogBreed) => {
     const nameFilter = nameInput ? nameInput : '[a-z]+';
     const regExp = new RegExp(nameFilter, 'gi');
-    return regExp.test(dogBreed.name);
+    if (!regExp.test(dogBreed.name)) return false;
+    
+    const matchesWeight = dogBreed.weight.metric.split(" ")[0] >= filters.minWeight && dogBreed.weight.metric.split(" ")[2] <= filters.maxWeight;
+    const matchesHeight = dogBreed.weight.metric.split(" ")[0] >= filters.minWeight && dogBreed.weight.metric.split(" ")[2] <= filters.maxWeight;
+    return matchesWeight;
   }
 
   return (
     <div className="App">
-      <h1> Hi, checkout the Search component!</h1>
-      <input onChange={e => handleNameChange(e)} type="text"></input>
+      <h1> A house is not a houm without a dog </h1>
+      <div>
+        <label> Enter a breed's name: </label>
+        <input onChange={e => handleNameChange(e)} type="text"></input>
+      </div>
+      <div>
+        <label> Weight as adult (in kg): </label>
+        From <input name="minWeight" onChange={e => handleFilterChange(e)} type="number"></input>
+        To <input name="maxWeight" onChange={e => handleFilterChange(e)} type="number"></input>
+      </div>
+      <div>
+        <label> Height as adult (in cm): </label>
+        From <input name="minHeight" onChange={e => handleFilterChange(e)} type="number"></input>
+        To <input name="maxHeight" onChange={e => handleFilterChange(e)} type="number"></input>
+      </div>
       <GridView data={dogs.filter(dog => matchesUserQuery(dog)).slice(0, 12)}/>
     </div>
   );
